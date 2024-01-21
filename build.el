5ee309392135035c6f234bac8b2096f6a5932296
VijayVinayaraj <vijayvinayaraj@gmail.com>
2024-01-21 16:56:24 +0530

    squash! remove with-footnotes

--- build.el
+++ build.el
@@ -1,4 +1,41 @@
-;;; this file build the website converting org files to html files
+;; ;;; this file build the website converting org files to html files
+;; (require 'package)
+;; (setq package-user-dir (expand-file-name "./.packages"))
+;; (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+;;                          ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; ;; Initialize the package system
+;; (package-initialize)
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+
+;; ;; Install dependencies
+;; (package-install 'htmlize)
+;; (require 'ox-publish)
+;; (setq org-html-validation-link nil
+;;       org-html-head-include-scripts nil       ;; Use our own scripts
+;;       org-html-head-include-default-style nil ;; Use our own styles
+;;       org-html-head "<link rel=\"stylesheet\" href=\"./style.css\"/>")
+;; (org-publish-reset-cache)
+;; (setq org-publish-project-alist
+;;       (list
+;;        (list "my-org-site"
+;;              :recursive t
+;;              :base-directory "./content"
+;;              :publishing-directory "./public"
+;;              :publishing-function 'org-html-publish-to-html
+;; 	      :with-author nil           ;; Don't include author name
+;;              :with-creator nil
+;;              :with-toc nil
+;;              :section-numbers nil       ;; Don't include section numbers
+;;              :time-stamp-file nil
+;; 	       )))
+
+;; ;; Generate the site output
+;; (org-publish-all t)
+;; (message "Build complete!")
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -11,27 +48,31 @@
 
 ;; Install dependencies
 (package-install 'htmlize)
+
+;; Load the publishing system
 (require 'ox-publish)
-(setq org-html-validation-link nil
-      
+
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"./style.css\"/>")
-(org-publish-reset-cache)
+      org-html-head "<link rel=\"stylesheet\" href=\"./style.css\" />")
+
+;; Define the publishing project
 (setq org-publish-project-alist
       (list
-       (list "my-org-site"
+       (list "org-site:main"
              :recursive t
              :base-directory "./content"
-             :publishing-directory "./public"
              :publishing-function 'org-html-publish-to-html
-	      :with-author nil           ;; Don't include author name
-             :with-creator nil
-             :with-toc nil
+             :publishing-directory "./public"
+             :with-author nil           ;; Don't include author name
+             :with-creator nil            ;; Include Emacs and Org versions in footer
+             :with-toc nil                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
-             :time-stamp-file nil
-	       )))
+             :time-stamp-file nil)))    ;; Don't include time stamp in file
 
 ;; Generate the site output
 (org-publish-all t)
+
 (message "Build complete!")
