de416bb595f3023a8bae90693fc6ac431c867d2c
VijayVinayaraj <vijayvinayaraj@gmail.com>
2024-01-21 16:46:04 +0530

    Add reset org publish cache

--- build.el
+++ build.el
@@ -17,6 +17,7 @@
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head "<link rel=\"stylesheet\" href=\"./style.css\"/>")
+(org-publish-reset-cache)
 (setq org-publish-project-alist
       (list
        (list "my-org-site"
